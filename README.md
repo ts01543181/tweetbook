7d4809c245c502f44c94cb7b48dc1fc827344fea
Joseph Cheng <joecheng950103@gmail.com>
2017-07-31 11:14:05 -0700

    hooked up server

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a social media app that lets users post their moments, read other people's moments, and talk to others in a public chat room. 
+Users can also post comments and dm other people.
+
+
+tech stack:
+- React
+- MySQL
+- Express
---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const parser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(parser.json())
+app.use(parser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, '../client/public')))
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(`Listening on port ${PORT}`)
+    }
+})
